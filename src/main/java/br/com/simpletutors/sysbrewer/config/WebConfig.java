07f50b3ae104756742d6897ca2dc0135920a9089
Simão Menezes <simaomenezesneto@gmail.com>
2016-08-17 21:00:12 -0300

    Configurando spring mvc, criando controller para cadastro de cerveja

---
+++ src/main/java/br/com/simpletutors/sysbrewer/config/WebConfig.java
@@ -0,0 +1,60 @@
+package br.com.simpletutors.sysbrewer.config;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
+import br.com.simpletutors.sysbrewer.controller.CervejasController;
+
+@Configuration // indica que é uma class de configuração
+@ComponentScan(basePackageClasses = {CervejasController.class}) //o sistema vai procurar os controle com referencia nesse controller
+@EnableWebMvc // indica que é um projeto web
+public class WebConfig extends WebMvcConfigurerAdapter  implements ApplicationContextAware{
+	
+	private ApplicationContext applicationContext;
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
+	}
+	
+	// resolve onde fica os templates/ paginas
+	@Bean
+	public ViewResolver viewResolver(){
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		resolver.setCharacterEncoding("UTF-8");
+		return resolver;
+	}
+	
+	// anotado com @Bean ele fica disponivel para o contexto do spring
+	@Bean
+	public TemplateEngine templateEngine(){
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setEnableSpringELCompiler(true);
+		engine.setTemplateResolver(templateResolver());
+		return engine;
+	}
+	
+	private ITemplateResolver templateResolver(){
+		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+		resolver.setApplicationContext(applicationContext);
+		resolver.setPrefix("classpath:/templates/");
+		resolver.setSuffix(".html");
+		resolver.setTemplateMode(TemplateMode.HTML);
+		return resolver;
+		
+	}	
+}
---
+++ src/main/java/br/com/simpletutors/sysbrewer/config/init/AppInitializer.java
@@ -0,0 +1,27 @@
+package br.com.simpletutors.sysbrewer.config.init;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import br.com.simpletutors.sysbrewer.config.WebConfig;
+
+public class AppInitializer	extends AbstractAnnotationConfigDispatcherServletInitializer{
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class<?>[]{WebConfig.class};
+	}
+	
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[]{"/"};
+	}
+
+}
