7ebe5ffc807eca496a5b808b986032f187045e68
Simão Menezes <simaomenezesneto@gmail.com>
2016-08-17 21:47:36 -0300

    Criando metodo para teste de envio de parametros para o servidor.

--- src/main/java/br/com/simpletutors/sysbrewer/controller/CervejasController.java
+++ src/main/java/br/com/simpletutors/sysbrewer/controller/CervejasController.java
@@ -2,13 +2,28 @@ package br.com.simpletutors.sysbrewer.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import br.com.simpletutors.sysbrewer.model.Cerveja;
+ 
 @Controller
 public class CervejasController {
 	
+	
+	// se for um get chama esse
 	@RequestMapping("/cervejas/novo")
 	public String novo(){
 		return "cerveja/CadastroCerveja";
 	}
+	
+	
+	// se for um post chama esse
+	@RequestMapping(value = "cervejas/novo", method = RequestMethod.POST)
+	public String cadastro(Cerveja cerveja){
+		System.out.println(">>>>>> cadastrar!: " + cerveja.getSku());
+		return "cerveja/CadastroCerveja";
+	}
+	
+	
 
 }
